cf351c87c29ab301d624428a6fe60f738a01a474
Pedro Alves <pta2002@pta2002.com>
2022-11-11 02:48:29 +0000

    spice up nvim-cmp

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -15,12 +15,38 @@ inputs: { pkgs, ... }:
 
       intellitab.enable = true;
       nix.enable = true;
-      bufferline.enable = true;
+      bufferline = {
+        enable = true;
+        diagnostics = "nvim_lsp";
+        separatorStyle = "slant";
+      };
       nvim-autopairs.enable = true;
       nvim-tree.enable = true;
 
       undotree.enable = true;
       surround.enable = true;
+
+      lspkind = {
+        enable = true;
+        mode = "symbol_text";
+        cmp.ellipsisChar = "…";
+        cmp.menu = {
+          buffer = "[Buffer]";
+          nvim_lsp = "[LSP]";
+          luasnip = "[LuaSnip]";
+          nvim_lua = "[Lua]";
+          latex_symbols = "[Latex]";
+        };
+        cmp.after = ''
+          function(entry, vim_item, kind)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. strings[1] .. " "
+            kind.menu = "   " .. strings[2]
+
+            return kind
+          end
+        '';
+      };
 
       lsp = {
         enable = true;
@@ -41,12 +67,19 @@ inputs: { pkgs, ... }:
 
       lsp-lines.enable = true;
 
+      lspsaga.enable = true;
+
       nvim-cmp = {
         enable = true;
         sources = [{ name = "nvim_lsp"; }];
         mappingPresets = [ "insert" ];
         mapping = {
           "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+        formatting.fields = [ "kind" "abbr" "menu" ];
+
+        window.completion = {
+          winhighlight = "Normal:Pmenu,FloatBordeR:Pmenu,Search:None";
         };
       };
 
@@ -97,6 +130,11 @@ inputs: { pkgs, ... }:
       "k" = "gk";
     };
 
+    extraConfigLua = ''
+      require("scope").setup()
+      require("colorizer").setup()
+    '';
+
     extraPlugins = with pkgs.vimPlugins; [
       vim-sleuth
       kanagawa-nvim
@@ -135,10 +173,64 @@ inputs: { pkgs, ... }:
           sha256 = "1cdmbk3kwkwvkl5jqg7g6fcg00ca6svwl8hkdr4hiz4qf04qx77y";
         };
       })
+      (pkgs.vimUtils.buildVimPlugin rec {
+        pname = "scope-nvim";
+        version = "2db6d31de8e3a98d2b41c0f0d1f5dc299ee76875";
+        src = pkgs.fetchFromGitHub {
+          owner = "tiagovla";
+          repo = "scope.nvim";
+          rev = version;
+          sha256 = "10l7avsjcgzh0s29az4zzskqcp9jw5xpvdiih02rf7c1j85zxm85";
+        };
+      })
       vim-endwise
       vim-terraform
+      nvim-colorizer-lua
     ];
 
     extraPackages = [ pkgs.xclip ];
+
+    highlight = {
+      PmenuSel = { bg = "#504945"; fg = "NONE"; };
+      Pmenu = { fg = "#ebdbb2"; bg = "#282828"; };
+
+      CmpItemAbbrDeprecated = { fg = "#d79921"; bg = "NONE"; strikethrough = true; };
+      CmpItemAbbrMatch = { fg = "#83a598"; bg = "NONE"; bold = true; };
+      CmpItemAbbrMatchFuzzy = { fg = "#83a598"; bg = "NONE"; bold = true; };
+      CmpItemMenu = { fg = "#b16286"; bg = "NONE"; italic = true; };
+
+      CmpItemKindField = { fg = "#fbf1c7"; bg = "#fb4934"; };
+      CmpItemKindProperty = { fg = "#fbf1c7"; bg = "#fb4934"; };
+      CmpItemKindEvent = { fg = "#fbf1c7"; bg = "#fb4934"; };
+
+      CmpItemKindText = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+      CmpItemKindEnum = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+      CmpItemKindKeyword = { fg = "#fbf1c7"; bg = "#b8bb26"; };
+
+      CmpItemKindConstant = { fg = "#fbf1c7"; bg = "#fe8019"; };
+      CmpItemKindConstructor = { fg = "#fbf1c7"; bg = "#fe8019"; };
+      CmpItemKindReference = { fg = "#fbf1c7"; bg = "#fe8019"; };
+
+      CmpItemKindFunction = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindStruct = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindClass = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindModule = { fg = "#fbf1c7"; bg = "#b16286"; };
+      CmpItemKindOperator = { fg = "#fbf1c7"; bg = "#b16286"; };
+
+      CmpItemKindVariable = { fg = "#fbf1c7"; bg = "#458588"; };
+      CmpItemKindFile = { fg = "#fbf1c7"; bg = "#458588"; };
+
+      CmpItemKindUnit = { fg = "#fbf1c7"; bg = "#d79921"; };
+      CmpItemKindSnippet = { fg = "#fbf1c7"; bg = "#d79921"; };
+      CmpItemKindFolder = { fg = "#fbf1c7"; bg = "#d79921"; };
+
+      CmpItemKindMethod = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+      CmpItemKindValue = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+      CmpItemKindEnumMember = { fg = "#fbf1c7"; bg = "#8ec07c"; };
+
+      CmpItemKindInterface = { fg = "#fbf1c7"; bg = "#83a598"; };
+      CmpItemKindColor = { fg = "#fbf1c7"; bg = "#83a598"; };
+      CmpItemKindTypeParameter = { fg = "#fbf1c7"; bg = "#83a598"; };
+    };
   };
 }
