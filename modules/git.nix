d0a34defbd8f4e9ed57d694c6663ce031e8cca6c
Pedro Alves <pta2002@pta2002.com>
2024-03-11 17:52:04 +0000

    add git aliases

--- modules/git.nix
+++ modules/git.nix
@@ -11,6 +11,19 @@
       core.autocrlf = "input";
       safe.directory = [ "/home/pta2002/nixos" ];
       rerere.enabled = true;
+      push.autoSetupRemote = true;
+
+      # Log here
+      alias.hlog = "log --oneline --graph -- .";
+      alias.glog = "log --oneline --graph";
+      alias.rbs = "rebase -i --autosquash";
+      alias.rbi = "rebase -i";
+      alias.rb = "rebase";
+      alias.ps = "push";
+      alias.fps = "push --force-with-lease";
+      alias.aa = "add -A";
+      alias.s = "status";
+      alias.cm = "commit";
     };
   };
 }
