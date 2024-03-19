2eb4d1e093f214c46af6793ef06eefa3b5a7545b
Pedro Alves <pta2002@pta2002.com>
2024-03-19 14:48:48 +0000

    fix grafana

--- modules/grafana.nix
+++ modules/grafana.nix
@@ -3,9 +3,9 @@
   services.grafana = {
     enable = true;
     domain = "grafana.pta2002.com";
-    port = 2342;
+    settings.server.http_port = 2342;
     addr = "127.0.0.1";
   };
 
-  services.argoWeb.ingress."grafana.pta2002.com" = "http://127.0.0.1:${config.services.grafana.port}";
+  services.argoWeb.ingress."grafana.pta2002.com" = "http://127.0.0.1:${builtins.toString config.services.grafana.settings.server.http_port}";
 }
