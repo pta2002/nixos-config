d380885b362c46a77f26838a2331aae4583cafba
Pedro Alves <pta2002@pta2002.com>
2025-01-24 10:50:58 +0000

    add flood

--- modules/transmission.nix
+++ modules/transmission.nix
@@ -1,11 +1,14 @@
-{ pkgs, config, ... }:
+{ config, ... }:
 {
   age.secrets.transmission = {
     file = ../secrets/transmission.age;
     owner = config.services.transmission.user;
   };
 
-  services.cloudflared.tunnels."mars".ingress."transmission.pta2002.com" = "http://localhost:9091";
+  services.cloudflared.tunnels."mars".ingress = {
+    "transmission.pta2002.com" = "http://localhost:9091";
+    "flood.pta2002.com" = "http://localhost:${builtins.toString config.services.flood.port}";
+  };
 
   services.transmission = {
     enable = true;
@@ -13,10 +16,14 @@
 
     home = "/var/lib/transmission";
 
-    settings.download-dir = "/mnt/data/torrents";
-    settings.incomplete-dir = "/mnt/data/torrents/.incomplete";
-    settings.rpc-authentication-required = true;
-    settings.rpc-host-whitelist = "transmission.pta2002.com";
+    settings = {
+      download-dir = "/mnt/data/torrents";
+      incomplete-dir = "/mnt/data/torrents/.incomplete";
+      rpc-authentication-required = true;
+      rpc-host-whitelist = "transmission.pta2002.com,localhost";
+      rpc-whitelist = "*";
+      download-queue-enabled = false;
+    };
 
     openPeerPorts = true;
 
@@ -29,6 +36,8 @@
   systemd.services.transmission = {
     requires = [ "mnt-data.mount" ];
   };
+
+  services.flood.enable = true;
 
   users.users.${config.services.transmission.user} = {
     home = config.services.transmission.home;
