6f6f1490ccd7c7d160387bfd2d15d455e7411e50
Pedro Alves <pta2002@pta2002.com>
2025-12-08 17:45:00 +0000

    jetson: stop rebuilding the nix package

--- flake.nix
+++ flake.nix
@@ -358,41 +358,51 @@
             };
 
             jetson = {
-              system = "aarch64";
+              system = "aarch64-linux";
               name = "jetson";
               stateVersion = "25.05";
+              specialArgs = { inherit inputs; };
               modules = [
                 jetpack-nixos.nixosModules.default
-                {
-                  nixpkgs.overlays = [
-                    (final: prev: {
-                      onnxruntime = prev.onnxruntime.override { ncclSupport = false; };
-                      python3 = prev.python3.override {
-                        packageOverrides = python-final: python-prev: {
-                          # The python package does not respect ncclSupport!
-                          onnxruntime = python-prev.onnxruntime.overrideAttrs {
-                            buildInputs = [
-                              prev.oneDNN
-                              prev.re2
-                              final.onnxruntime.protobuf
-                              final.onnxruntime
-                            ]
-                            ++ lib.optionals final.onnxruntime.passthru.cudaSupport (
-                              with final.onnxruntime.passthru.cudaPackages;
-                              [
-                                libcublas # libcublasLt.so.XX libcublas.so.XX
-                                libcurand # libcurand.so.XX
-                                libcufft # libcufft.so.XX
-                                cudnn # libcudnn.soXX
-                                cuda_cudart # libcudart.so.XX
+                (
+                  { inputs, ... }:
+                  {
+                    nixpkgs.overlays = [
+                      (final: prev: {
+                        onnxruntime = prev.onnxruntime.override { ncclSupport = false; };
+                        python3-cuda = prev.python3.override {
+                          packageOverrides = python-final: python-prev: {
+                            # The python package does not respect ncclSupport!
+                            onnxruntime = python-prev.onnxruntime.overrideAttrs {
+                              buildInputs = [
+                                prev.oneDNN
+                                prev.re2
+                                final.onnxruntime.protobuf
+                                final.onnxruntime
                               ]
-                            );
+                              ++ lib.optionals final.onnxruntime.passthru.cudaSupport (
+                                with final.onnxruntime.passthru.cudaPackages;
+                                [
+                                  libcublas # libcublasLt.so.XX libcublas.so.XX
+                                  libcurand # libcurand.so.XX
+                                  libcufft # libcufft.so.XX
+                                  cudnn # libcudnn.soXX
+                                  cuda_cudart # libcudart.so.XX
+                                ]
+                              );
+                            };
                           };
                         };
-                      };
-                    })
-                  ];
-                }
+                      })
+                    ];
+
+                    # Use standard nix package to avoid rebuilds due to cudaSupport
+                    nix.package =
+                      (import inputs.nixpkgs {
+                        system = "aarch64-linux";
+                      }).nix;
+                  }
+                )
               ];
               roles = [
                 "immich"
