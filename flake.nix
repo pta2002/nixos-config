1ab5192c0289cb44df25897cbbadf14d94e714f6
Pedro Alves <pta2002@pta2002.com>
2026-01-05 10:09:32 +0000

    nix: remove cuda-maintainers cache
    This is overridden by the flox cache

--- flake.nix
+++ flake.nix
@@ -57,7 +57,6 @@
     substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org"
-      "https://cuda-maintainers.cachix.org"
       "https://nixos-raspberrypi.cachix.org"
       "https://cache.flox.dev"
       "https://attic.c.pta2002.com/homelab-main"
@@ -65,7 +64,6 @@
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
       "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
       "homelab-main:h4K5u2G2cDloc+KyelM1vHDSGh6JzfNfK2uB31x76+s="
